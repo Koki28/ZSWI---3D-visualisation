1feef8dc6c5586b6a4d2e16545f4b93953d21209
Iopi <33093127+Iopi@users.noreply.github.com>
2018-04-20 17:39:57 +0200

    Editor 4

--- ZSWI - 3D visualisation/Assets/Game Jam Template/Menu/ShowPanels.cs
+++ ZSWI - 3D visualisation/Assets/Game Jam Template/Menu/ShowPanels.cs
@@ -8,7 +8,12 @@ public class ShowPanels : MonoBehaviour
     public GameObject optionsPanel;                         //Store a reference to the Game Object OptionsPanel 
     public GameObject optionsTint;                          //Store a reference to the Game Object OptionsTint 
     public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
+<<<<<<< HEAD
     public GameObject scenePanel;							//Store a reference to the Game Object ScenePanel 
+=======
+    public GameObject scenePanel;                           //Store a reference to the Game Object ScenePanel 
+>>>>>>> 9a88f2abcf6d8b66247c983d9ef1dc9f1d5e8492
+    public GameObject aboutPanel;							//Store a reference to the Game Object AboutPanel 
 
 
     private GameObject activePanel;
@@ -43,7 +48,7 @@ public class ShowPanels : MonoBehaviour
 
     }
 
-    //Call this function to activate and display the Options panel during the main menu
+    //Call this function to activate and display the Scene panel during the main menu
     public void ShowScnenePanel()
     {
         scenePanel.SetActive(true);
@@ -52,13 +57,27 @@ public class ShowPanels : MonoBehaviour
 
     }
 
-    //Call this function to deactivate and hide the Options and Scene panel during the main menu
-    public void HideOptionsPanel()
+    //Call this function to activate and display the About panel during the main menu
+    public void ShowAboutPanel()
+    {
+        aboutPanel.SetActive(true);
+        menuPanel.SetActive(false);
+        SetSelection(aboutPanel);
+
+    }
+
+<<<<<<< HEAD
+    //Call this function to deactivate and hide the About, Options and Scene panel during the main menu
+=======
+    //Call this function to deactivate and hide the Options, Scene and About panel during the main menu
+>>>>>>> 9a88f2abcf6d8b66247c983d9ef1dc9f1d5e8492
+    public void HideOtherPanels()
     {
         menuPanel.SetActive(true);
         optionsPanel.SetActive(false);
         optionsTint.SetActive(false);
         scenePanel.SetActive(false);
+        aboutPanel.SetActive(false);
     }
 
     //Call this function to activate and display the main menu panel during the main menu
