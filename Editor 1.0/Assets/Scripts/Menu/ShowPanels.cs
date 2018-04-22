40564eeb48c6d5fed4997680c7e81b222dde7206
Iopi <33093127+Iopi@users.noreply.github.com>
2018-04-22 14:30:34 +0200

    eda

---
+++ Editor 1.0/Assets/Scripts/Menu/ShowPanels.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ShowPanels : MonoBehaviour
+{
+
+    public GameObject optionsPanel;                         //Store a reference to the Game Object OptionsPanel 
+    public GameObject optionsTint;                          //Store a reference to the Game Object OptionsTint 
+    public GameObject menuPanel;                            //Store a reference to the Game Object MenuPanel 
+    public GameObject scenePanel;                           //Store a reference to the Game Object ScenePanel 
+    public GameObject aboutPanel;                           //Store a reference to the Game Object AboutPanel 
+
+    private GameObject activePanel;
+    private MenuObject activePanelMenuObject;
+    private EventSystem eventSystem;
+
+
+
+    private void SetSelection(GameObject panelToSetSelected)
+    {
+
+        activePanel = panelToSetSelected;
+        activePanelMenuObject = activePanel.GetComponent<MenuObject>();
+        if (activePanelMenuObject != null)
+        {
+            activePanelMenuObject.SetFirstSelected();
+        }
+    }
+
+    public void Start()
+    {
+        SetSelection(menuPanel);
+    }
+
+    //Call this function to activate and display the Options panel during the main menu
+    public void ShowOptionsPanel()
+    {
+        optionsPanel.SetActive(true);
+        optionsTint.SetActive(true);
+        menuPanel.SetActive(false);
+        SetSelection(optionsPanel);
+
+    }
+
+    //Call this function to activate and display the Options panel during the main menu
+    public void ShowScnenePanel()
+    {
+        scenePanel.SetActive(true);
+        menuPanel.SetActive(false);
+        SetSelection(scenePanel);
+
+    }
+
+    //Call this function to activate and display the About panel during the main menu
+    public void ShowAboutPanel()
+    {
+        aboutPanel.SetActive(true);
+        menuPanel.SetActive(false);
+        SetSelection(aboutPanel);
+
+    }
+
+    //Call this function to deactivate and hide the About, Options and Scene panel during the main menu
+    public void HideOtherPanels()
+    {
+        menuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        optionsTint.SetActive(false);
+        scenePanel.SetActive(false);
+    }
+
+    //Call this function to activate and display the main menu panel during the main menu
+    public void ShowMenu()
+    {
+        menuPanel.SetActive(true);
+        SetSelection(menuPanel);
+    }
+
+    //Call this function to deactivate and hide the main menu panel during the main menu
+    public void HideMenu()
+    {
+        menuPanel.SetActive(false);
+
+    }
+}
