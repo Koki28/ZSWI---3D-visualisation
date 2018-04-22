ac68a9282a188904eed4dceccd70e00a5e38999e
Koki28 <tomkratos@seznam.cz>
2018-04-22 15:13:17 +0200

    Revert "revert Editor 3.2"
    This reverts commit 9a88f2abcf6d8b66247c983d9ef1dc9f1d5e8492.

--- ZSWI - 3D visualisation/Assets/Game Jam Template/Menu/ShowPanels.cs
+++ ZSWI - 3D visualisation/Assets/Game Jam Template/Menu/ShowPanels.cs
@@ -7,24 +7,11 @@ public class ShowPanels : MonoBehaviour
 
     public GameObject optionsPanel;                         //Store a reference to the Game Object OptionsPanel 
     public GameObject optionsTint;                          //Store a reference to the Game Object OptionsTint 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public GameObject menuPanel;                            //Store a reference to the Game Object MenuPanel 
-    public GameObject scenePanel;                           //Store a reference to the Game Object ScenePanel 
-    public GameObject aboutPanel;                           //Store a reference to the Game Object AboutPanel 
-=======
-=======
 >>>>>>> parent of 1feef8d... Editor 4
-=======
->>>>>>> parent of 9a88f2a... revert Editor 3.2
-=======
->>>>>>> parent of 2351622... Editor 3.2
     public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
-    public GameObject scenePanel;							//Store a reference to the Game Object ScenePanel 
+    public GameObject scenePanel;                           //Store a reference to the Game Object ScenePanel 
+    public GameObject aboutPanel;							//Store a reference to the Game Object AboutPanel 
 
->>>>>>> parent of 1feef8d... Editor 4
 
     private GameObject activePanel;
     private MenuObject activePanelMenuObject;
@@ -58,7 +45,7 @@ public class ShowPanels : MonoBehaviour
 
     }
 
-    //Call this function to activate and display the Options panel during the main menu
+    //Call this function to activate and display the Scene panel during the main menu
     public void ShowScnenePanel()
     {
         scenePanel.SetActive(true);
@@ -67,10 +54,6 @@ public class ShowPanels : MonoBehaviour
 
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     //Call this function to activate and display the About panel during the main menu
     public void ShowAboutPanel()
     {
@@ -80,29 +63,14 @@ public class ShowPanels : MonoBehaviour
 
     }
 
-    //Call this function to deactivate and hide the About, Options and Scene panel during the main menu
+    //Call this function to deactivate and hide the Options, Scene and About panel during the main menu
     public void HideOtherPanels()
-=======
-    //Call this function to deactivate and hide the Options and Scene panel during the main menu
-    public void HideOptionsPanel()
->>>>>>> parent of 1feef8d... Editor 4
-=======
-    //Call this function to deactivate and hide the Options and Scene panel during the main menu
-    public void HideOptionsPanel()
->>>>>>> parent of 1feef8d... Editor 4
-=======
-    //Call this function to deactivate and hide the Options and Scene panel during the main menu
-    public void HideOptionsPanel()
->>>>>>> parent of 9a88f2a... revert Editor 3.2
-=======
-    //Call this function to deactivate and hide the Options and Scene panel during the main menu
-    public void HideOptionsPanel()
->>>>>>> parent of 2351622... Editor 3.2
     {
         menuPanel.SetActive(true);
         optionsPanel.SetActive(false);
         optionsTint.SetActive(false);
         scenePanel.SetActive(false);
+        aboutPanel.SetActive(false);
     }
 
     //Call this function to activate and display the main menu panel during the main menu
