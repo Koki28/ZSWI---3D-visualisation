f6b8fee50f06e094c116ef384a1cfa056645337f
Iopi <33093127+Iopi@users.noreply.github.com>
2018-04-24 11:05:43 +0200

    Eda III

---
+++ Editor 1.0/Assets/Scripts/Menu/CallingPrefabs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallingPrefabs : MonoBehaviour
+{
+
+    public GameObject cube;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickCube()
+    {
+        //transform.position = new Vector3(508.5f, 191.5f, 5.0f);
+        Instantiate(cube, new Vector3(0.0f, 0.0f, -5.0f), transform.rotation);
+
+    }
+}
