aa95581a888b211391439cbcb65f521e2cb93ea0
Iopi <33093127+Iopi@users.noreply.github.com>
2018-04-30 15:00:18 +0200

    Eda 3

---
+++ Editor 1.0/Assets/HierarchyView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HierarchyView : MonoBehaviour {
+    public Text myText;
+    public string display;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        List<GameObject> listGO = CallingPrefabs.getGOList();
+        myText.text = "This is my text";
+        //display = display.ToString() + myText.ToString() + "\n";
+        foreach (GameObject prefab in listGO) {
+            
+           }
+	}
+
+
+}
--- Editor 1.0/Assets/Scripts/Menu/CallingPrefabs.cs
+++ Editor 1.0/Assets/Scripts/Menu/CallingPrefabs.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class CallingPrefabs : MonoBehaviour
 {
 
-    public GameObject cube;
+    public GameObject prefab;
+    public GameObject shelf;
+    public Transform hiearchyPanel;
+    private static List<GameObject> listGO;
 
     // Use this for initialization
-    void Start()
+    void Start() 
     {
-
+        listGO = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -21,8 +24,26 @@ public class CallingPrefabs : MonoBehaviour
 
     public void OnClickCube()
     {
-        //transform.position = new Vector3(508.5f, 191.5f, 5.0f);
-        Instantiate(cube, new Vector3(0.0f, 0.0f, -5.0f), transform.rotation);
+        Instantiate(prefab, new Vector3(0.0f, 0.0f, -5.0f), transform.rotation);
+        listGO.Add((GameObject)Instantiate(prefab, new Vector3(0.0f, 0.0f, -5.0f), Quaternion.identity));
+        // CreateA();
+    }
 
+    public void OnClickShelf()
+    {
+        Instantiate(shelf, new Vector3(0.0f, 0.0f, -5.0f), transform.rotation);
+        listGO.Add((GameObject)Instantiate(shelf, new Vector3(0.0f, 0.0f, -5.0f), Quaternion.identity));
+        // CreateA();
+    }
+
+    public static List<GameObject> getGOList()
+    {
+        return listGO;
+    }
+
+    void CreateA()
+    {
+        GameObject a = (GameObject)Instantiate(prefab);
+        a.transform.SetParent(hiearchyPanel.transform, false);
     }
 }
