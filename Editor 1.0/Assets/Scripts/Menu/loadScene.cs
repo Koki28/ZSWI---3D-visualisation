23972e72b91a93c96f5d6074234891874b635b02
Totina <tereza.tothova96@gmail.com>
2018-05-14 22:33:21 +0200

    simpleJson imported

---
+++ Editor 1.0/Assets/Scripts/Menu/loadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+
+public class loadScene : MonoBehaviour {
+
+	public GameObject belt;
+	public GameObject shelf;
+	public GameObject table;
+	public GameObject forklift;
+	public GameObject truck;
+	public GameObject floor;
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
